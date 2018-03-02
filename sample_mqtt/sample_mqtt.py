f6bdcdc5b2906127b3cf24741e2f72fe42b26a9b
Benjamin Borbe <bborbe@rocketnews.de>
2018-03-02 13:41:48 +0100

    handle errors

--- sample_mqtt/sample_mqtt.py
+++ sample_mqtt/sample_mqtt.py
@@ -3,31 +3,46 @@
 import time
 import paho.mqtt.client as paho
 
-broker = 'localhost'
-queue = 'house/bulb1'
-username = "user"
-password = "pass"
 
 def on_message(client, userdata, message):
     print('received message = {}'.format(str(message.payload.decode('utf-8'))))
 
 
+broker = 'localhost'
+queue = 'house/bulb1'
+username = "user"
+password = "pass"
+
 client = paho.Client('client-001')
 client.on_message = on_message
 client.username_pw_set(username=username, password=password)
-
 print('connecting to broker {}'.format(broker))
-client.connect(broker)
-client.loop_start()
-
+res = client.connect(broker)
+if res != paho.MQTT_ERR_SUCCESS:
+    print('connect failed: {}'.format(paho.error_string(res)))
+    exit(1)
+res = client.loop_start()
+if res == paho.MQTT_ERR_INVAL:
+    print('start loop failed: {}'.format(paho.error_string(res)))
+    exit(1)
 print('subscribing to {}'.format(queue))
-client.subscribe(queue)
+res = client.subscribe(queue)
+if res[0] != paho.MQTT_ERR_SUCCESS:
+    print('subscribe failed: {}'.format(paho.error_string(res)))
+    exit(1)
 time.sleep(2)
-
 print('publishing to {}'.format(queue))
-client.publish(queue, 'hello world')
+res = client.publish(queue, 'hello world')
+if res[0] != paho.MQTT_ERR_SUCCESS:
+    print('publish failed: {}'.format(paho.error_string(res)))
+    exit(1)
 time.sleep(4)
-
+res = client.disconnect()
+if res != paho.MQTT_ERR_SUCCESS:
+    print('disconnect failed: {}'.format(paho.error_string(res)))
+    exit(1)
+res = client.loop_stop()
+if res == paho.MQTT_ERR_INVAL:
+    print('stop loop failed: {}'.format(paho.error_string(res)))
+    exit(1)
 print('done')
-client.disconnect()
-client.loop_stop()
