ee8a65166470446584a535333eef0af7870b6aa5
Benjamin Borbe <bborbe@rocketnews.de>
2018-03-02 11:27:03 +0100

    add mqtt sample

---
+++ sample_mqtt/README.md
@@ -0,0 +1,13 @@
+# MQTT
+
+## Start MQTT server
+
+```
+docker run -it -p 1883:1883 -p 9001:9001 eclipse-mosquitto
+```
+
+## Run sample
+
+```
+./sample_mqtt.py
+```
--- sample_mqtt/sample_mqtt.py
+++ sample_mqtt/sample_mqtt.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python3
 
-print('hello')
+import time
+import paho.mqtt.client as paho
+
+broker = 'localhost'
+queue = 'house/bulb1'
+
+
+# define callback
+def on_message(client, userdata, message):
+    print('received message = {}'.format(str(message.payload.decode('utf-8'))))
+
+
+client = paho.Client('client-001')
+client.on_message = on_message
+
+print('connecting to broker {}'.format(broker))
+client.connect(broker)
+client.loop_start()
+
+print('subscribing to {}'.format(queue))
+client.subscribe(queue)
+time.sleep(2)
+
+print('publishing to {}'.format(queue))
+client.publish(queue, 'hello world')
+time.sleep(4)
+
+print('done')
+client.disconnect()  # disconnect
+client.loop_stop()  # stop loop
